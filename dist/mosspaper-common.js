5c45c4109f992c21726cc374da85f49d20dba39e
cfong <cfong@mosspaper.com>
2015-09-01 20:59:11 -0700

    update

--- dist/mosspaper-common.js
+++ dist/mosspaper-common.js
@@ -1,33 +1,34 @@
 (function (angular) {
 
-  // Create all modules and define dependencies to make sure they exist
-  // and are loaded in the correct order to satisfy dependency injection
-  // before all nested files are concatenated by Gulp
+    // Create all modules and define dependencies to make sure they exist
+    // and are loaded in the correct order to satisfy dependency injection
+    // before all nested files are concatenated by Gulp
+    angular.module('mosspaperCommon', []);
 
-  // Config
-  angular.module('mosspaperCommon.config', [])
-      .value('mosspaperCommon.config', {
-          debug: true
-      });
+    // Config
+    angular.module('mosspaperCommon.config', [])
+        .value('mosspaperCommon.config', {
+            debug: true
+        });
 
-  // Modules
-  angular.module('mosspaperCommon.directives', []);
-  angular.module('mosspaperCommon.filters', []);
-  angular.module('mosspaperCommon.services', []);
-  angular.module('mosspaperCommon',
-      [
-          'mosspaperCommon.config',
-          'mosspaperCommon.directives',
-          'mosspaperCommon.filters',
-          'mosspaperCommon.services',
-          'ngResource',
-          'ngCookies',
-          'ngSanitize'
-      ]);
+    // Modules
+    angular.module('mosspaperCommon.directives', []);
+    angular.module('mosspaperCommon.filters', []);
+    angular.module('mosspaperCommon.services', []);
+    angular.module('mosspaperCommon',
+        [
+            'mosspaperCommon.config',
+            'mosspaperCommon.directives',
+            'mosspaperCommon.filters',
+            'mosspaperCommon.services',
+            'ngResource',
+            'ngCookies',
+            'ngSanitize'
+        ]);
 
 })(angular);
 
-angular.module('mosspaperCommon')
+angular.module('mosspaperCommon.services')
     .factory('Utils', function ($q) {
         return {
             isImage: function (src) {
