235cfdd1adc346218635231f6e9d73cb5092436b
cfong <cfong@mosspaper.com>
2015-09-01 17:34:15 -0700

    generate dist

---
+++ dist/mosspaper-common.js
@@ -0,0 +1,34 @@
+(function (angular) {
+
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
+
+  // Config
+  angular.module('mosspaperCommon.config', [])
+      .value('mosspaperCommon.config', {
+          debug: true
+      });
+
+  // Modules
+  angular.module('mosspaperCommon.directives', []);
+  angular.module('mosspaperCommon.filters', []);
+  angular.module('mosspaperCommon.services', []);
+  angular.module('mosspaperCommon',
+      [
+          'mosspaperCommon.config',
+          'mosspaperCommon.directives',
+          'mosspaperCommon.filters',
+          'mosspaperCommon.services',
+          'ngResource',
+          'ngCookies',
+          'ngSanitize'
+      ]);
+
+})(angular);
+
+angular.module('mosspaperCommon')
+    .factory('Utils', function ($q) {
+
+    });
+
