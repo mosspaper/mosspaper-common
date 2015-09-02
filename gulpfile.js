1fbab669320cf45521166037d7b0c66ca4c9c1cd
cfong <cfong@mosspaper.com>
2015-09-01 17:53:35 -0700

    common utils

--- gulpfile.js
+++ gulpfile.js
@@ -20,34 +20,34 @@ var sourceDirectory = path.join(rootDirectory, './src');
 
 var sourceFiles = [
 
-  // Make sure module files are handled first
-  path.join(sourceDirectory, '/**/*.module.js'),
+    // Make sure module files are handled first
+    path.join(sourceDirectory, '/**/*.module.js'),
 
-  // Then add all JavaScript files
-  path.join(sourceDirectory, '/**/*.js')
+    // Then add all JavaScript files
+    path.join(sourceDirectory, '/**/*.js')
 ];
 
 var lintFiles = [
-  'gulpfile.js',
-  // Karma configuration
-  'karma-*.conf.js'
+    'gulpfile.js',
+    // Karma configuration
+    'karma-*.conf.js'
 ].concat(sourceFiles);
 
-gulp.task('build', function() {
-  gulp.src(sourceFiles)
-    .pipe(plumber())
-    .pipe(concat('mosspaper-common.js'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(uglify())
-    .pipe(rename('mosspaper-common.min.js'))
-    .pipe(gulp.dest('./dist'));
+gulp.task('build', function () {
+    gulp.src(sourceFiles)
+        .pipe(plumber())
+        .pipe(concat('mosspaper-common.js'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(uglify())
+        .pipe(rename('mosspaper-common.min.js'))
+        .pipe(gulp.dest('./dist'));
 });
 
 /**
  * Process
  */
 gulp.task('process-all', function (done) {
-  runSequence('jshint', 'test-src', 'build', done);
+    runSequence('jshint', 'test-src', 'build', done);
 });
 
 /**
@@ -55,51 +55,52 @@ gulp.task('process-all', function (done) {
  */
 gulp.task('watch', function () {
 
-  // Watch JavaScript files
-  gulp.watch(sourceFiles, ['process-all']);
+    // Watch JavaScript files
+    gulp.watch(sourceFiles, ['process-all']);
 });
 
 /**
  * Validate source JavaScript
  */
 gulp.task('jshint', function () {
-  return gulp.src(lintFiles)
-    .pipe(plumber())
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+    return gulp.src(lintFiles)
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint({expr: true}))
+        .pipe(jshint.reporter('jshint-stylish'));
+        //.pipe(jshint.reporter('fail'));
 });
 
 /**
  * Run test once and exit
  */
 gulp.task('test-src', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-src.conf.js',
-    singleRun: true
-  }, done);
+    karma.start({
+        configFile: __dirname + '/karma-src.conf.js',
+        singleRun: true
+    }, done);
 });
 
 /**
  * Run test once and exit
  */
 gulp.task('test-dist-concatenated', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-dist-concatenated.conf.js',
-    singleRun: true
-  }, done);
+    karma.start({
+        configFile: __dirname + '/karma-dist-concatenated.conf.js',
+        singleRun: true
+    }, done);
 });
 
 /**
  * Run test once and exit
  */
 gulp.task('test-dist-minified', function (done) {
-  karma.start({
-    configFile: __dirname + '/karma-dist-minified.conf.js',
-    singleRun: true
-  }, done);
+    karma.start({
+        configFile: __dirname + '/karma-dist-minified.conf.js',
+        singleRun: true
+    }, done);
 });
 
 gulp.task('default', function () {
-  runSequence('process-all', 'watch');
+    runSequence('process-all', 'watch');
 });
